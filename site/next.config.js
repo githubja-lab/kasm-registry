4b05284744a19275557a6a2e40849d7503bfc5a5
Jorge Adam <65267650+githubja-lab@users.noreply.github.com>
2023-04-21 12:56:31 -0700

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,8 +2,8 @@
 
 const nextConfig = {
   env: {
-    name: 'Company 3 KASM',
-    description: 'Kasm supported workspaces Company 3.',
+    name: 'CO3 KASM',
+    description: 'Kasm supported workspaces CO3.',
     icon: 'https://githubja-lab.github.io/kasm-registry/1.0/image.png',
     listUrl: 'https://githubja-lab.github.io/kasm-registry/',  
     contactUrl: 'https://github.com/githubja-lab/kasm-registry/issues',
