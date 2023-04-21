f0cfc90f7d1b82ac94d9660b5f99e9bdf3e2d726
Jorge Adam <65267650+githubja-lab@users.noreply.github.com>
2023-04-21 12:43:47 -0700

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,8 +2,8 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Company 3 KASM',
+    description: 'Kasm supported workspaces Company 3.',
     icon: '/img/logo.svg',
     listUrl: 'https://registry.kasmweb.com/',
     contactUrl: 'https://kasmweb.com/support',
